192a7b1016d246683e948b74d314e5510f503c3f
haubaustin <haubaustin@gmail.com>
2022-06-01 13:09:24 -0700

    Adjusted Media Query for welcome image

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -4,27 +4,22 @@ const WelcomePage = () => {
     return (
     <div className="WelcomePage">
         <div className="welcomePageImage">
-            <div className="welcomePageImage1"></div>
-        </div>
-        <div className="welcomePageText">
-            <h3>
-                Welcome to inVINtory
-            </h3>
-            <p>
-                inVINtory is a wine tracking and visualization application designed to provide users with the ability to search and view all the wine they currently posses. 
-            </p>
-            <p>
-                No longer will you have to continually pull bottles out of your fridge and check labels over and over again. Just check your app and see where you had put the bottle when you stored it
-            </p>
-            <p>
-                Begin by signing up or logging in below
-            </p>
-                <button className="welcomePageButton" onClick={(e) => {
-                    e.preventDefault()
-                    window.location.href="/register"
-                }}>
-                    Signup/Signin
-                </button>
+            <div className="welcomePageText">
+                <h2>
+                    Welcome to inVINtory
+                </h2>
+                <p className="welcomePageParagraph">
+                    inVINtory is a wine tracking and visualization application designed to provide users with the ability to search and view all the wine they currently posses. <br/>
+                    No longer will you have to continually pull bottles out of your fridge and check labels over and over again. Just check your app and see where you had put the bottle when you stored it<br/>
+                    Begin by signing up or logging in below
+                </p>
+                    <button className="welcomePageButton" onClick={(e) => {
+                        e.preventDefault()
+                        window.location.href="/register"
+                    }}>
+                        Signup/Signin
+                    </button>
+            </div>
         </div>
     </div>
     )
